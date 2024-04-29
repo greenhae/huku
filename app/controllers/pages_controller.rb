2ef4c9eba3869ce39a6155b46971086d58f696e7
greenhae <greenbeenice@gmail.com>
2024-04-29 20:39:12 +0900

    Add some DB operations--CRUD.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,8 @@ class PagesController < ApplicationController
     end
 
     def about
+        @hello_test = "Hello variable test !"
+        render template: 'pages/about'
+        
     end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Huku
 
     # Configuration for the application, engines, and railties goes here.
     #
+    # Set the time zone to 'Tokyo' for local Japan time
+    config.time_zone = "Tokyo"
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
